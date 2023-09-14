04ed33859e9600595ab20636a366f08b50305a01
ayse gungormez <aysegngrmz.91@windowslive.com>
2023-09-14 16:37:09 +0300

    routes for clients and transactions

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :clients do
+    resources :transactions
+  end
+
+  root "transactions#new"
 end
